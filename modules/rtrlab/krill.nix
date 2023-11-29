b091c5f0be90af485d8711c1b154e183dc282f32
revol-xut <revol-xut@protonmail.com>
2023-11-29 14:20:36 +0100

    adding nginx for the krill

--- modules/rtrlab/krill.nix
+++ modules/rtrlab/krill.nix
@@ -1,11 +1,22 @@
 {pkgs, config, lib, ...}: {
   virtualisation.docker.enable = true;
 
-  systemd.services."krill" = {
-    enable = true;
-    wantedBy = [ "multi-user.target" ]; 
-    script = ''
-      ${pkgs.krill}/bin/krill --config ${./krill.conf}
-    '';
+  #systemd.services."krill" = {
+  #  enable = true;
+  #  wantedBy = [ "multi-user.target" ]; 
+  #  script = ''
+  #    ${pkgs.krill}/bin/krill --config ${./krill.conf}
+  #  '';
+  #};
+
+  services.nginx = {
+
+
+    virtualHosts."krill.rtrlab.tanneberger.me" = {
+      enableACME = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:3000";
+      };
+    };
   };
 }
