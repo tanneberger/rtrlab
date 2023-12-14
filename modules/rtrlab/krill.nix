c7bc1f24788d4a57137ec6e780fc3d8b3bb33cf1
revol-xut <revol-xut@protonmail.com>
2023-12-14 14:31:29 +0100

    setting proper env variables

--- modules/rtrlab/krill.nix
+++ modules/rtrlab/krill.nix
@@ -2,12 +2,14 @@
 let
   http-host = "127.0.0.1";
   http-port = 3000;
+  token = "274oSaJhC5BMmvezU7VVhbVC0DeXgKIMuqUITtYx3xRplMRTJpfQ++CokGEK1BgI4Y8c8TcIc7EYBQDw5/enNLQWR6d+deXAGpe7LZHBZw1r7yX/AIRMO6zsoc60+SWHh3gJt8Lm/wCmyodz6aoZpkR+m7yT+IQ1n+Z9PEEbt9gXr6G2jhI7gXWUag695V8zlQI+XpUmgmDpLcfaQYu+5U0wgEp5IB+Sixk4n1y/9NweQTKZ8SBI9w/kP/ZQ";
+
 
   config-file = pkgs.writeText "krill.conf" ''
     storage_uri = "/var/lib/krill/data/"
     tls_keys_dir = "/var/lib/krill/data/ssl"
     log_level = "warn"
-    admin_token = "274oSaJhC5BMmvezU7VVhbVC0DeXgKIMuqUITtYx3xRplMRTJpfQ++CokGEK1BgI4Y8c8TcIc7EYBQDw5/enNLQWR6d+deXAGpe7LZHBZw1r7yX/AIRMO6zsoc60+SWHh3gJt8Lm/wCmyodz6aoZpkR+m7yT+IQ1n+Z9PEEbt9gXr6G2jhI7gXWUag695V8zlQI+XpUmgmDpLcfaQYu+5U0wgEp5IB+Sixk4n1y/9NweQTKZ8SBI9w/kP/ZQ"
+    admin_token = "${token}"
     ip = "${http-host}"
     port = ${toString http-port}
     https_mode = "disable"
@@ -17,6 +19,12 @@ let
   '';
 
 in {
+  environment.systemPackages = with pkgs; [ krill ]; 
+  environment.variables  = {
+	KRILL_CLI_TOKEN = "${token}";
+	KRILL_CLI_SERVER = "https://rtrlab.tanneberger.me/";
+	KRILL_CLI_MY_CA = "rtrlab";
+  };
 
   systemd.services."krill" = {
     enable = true;
