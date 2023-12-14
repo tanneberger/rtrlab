c1d677c74a2460e22bc5cfa5662a0d18e83aa413
revol-xut <revol-xut@protonmail.com>
2023-12-14 01:49:26 +0100

    fixing config

--- modules/rtrlab/krill.nix
+++ modules/rtrlab/krill.nix
@@ -5,11 +5,11 @@ let
 
   config-file = pkgs.writeText "krill.conf" ''
     storage_uri = "/var/lib/krill/data/"
-    tls_keys_dir = $storage_uri/ssl
+    tls_keys_dir = "/var/lib/krill/data/ssl"
     log_level = "warn"
     admin_token = "274oSaJhC5BMmvezU7VVhbVC0DeXgKIMuqUITtYx3xRplMRTJpfQ++CokGEK1BgI4Y8c8TcIc7EYBQDw5/enNLQWR6d+deXAGpe7LZHBZw1r7yX/AIRMO6zsoc60+SWHh3gJt8Lm/wCmyodz6aoZpkR+m7yT+IQ1n+Z9PEEbt9gXr6G2jhI7gXWUag695V8zlQI+XpUmgmDpLcfaQYu+5U0wgEp5IB+Sixk4n1y/9NweQTKZ8SBI9w/kP/ZQ"
     ip = "${http-host}"
-    port = ${toString http-port}          
+    port = ${toString http-port}
     https_mode = "disable"
     service_uri = "https://rtrlab.tanneberger.me/krill"
     ca_refresh_seconds = 5400
