a64140c52b68a134c2aea72ee28b13688181e218
revol-xut <revol-xut@protonmail.com>
2024-01-08 16:48:59 +0100

    using rtr version 2 for rtrlib fishy server

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,7 @@ async fn announce_config(stream: &mut rtr::RtrStream, x: &mut State) {
     .expect("cannot parse topology");
 
     // version id for header
-    let version = 1;
+    let version = 2;
     let start_serial = 42;
     // construct current state with serial
     let mut session_state = State::new_with_serial(Serial::from_be(start_serial));
